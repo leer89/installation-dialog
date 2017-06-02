9d420847d17efb26b9a26e78d44688479fe49739
leer89 <leer89@gmail.com>
2017-06-01 23:10:11 -0400

    finitial/linitial scanner output print initials

---
+++ src/leer6/MainApp.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leer6;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author RandyLee
+ */
+public class MainApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JOptionPane.showMessageDialog(null, 
+                "Before starting the installation, " +
+                        "shut down all applications.");
+    }
+    
+}
